35c2e6cbd2a456370c44920ea84feceb8bfe49ba
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-04-09 11:24:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,44 @@ module "network" {
             subnet_region         = "us-central1"
         },]
 }
+  
+  
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = var.network_name
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
 
   
 
