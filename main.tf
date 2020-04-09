1f27ef58af01c6ae4cdd8ba89ebfb491d74d574f
Alex Harness <9669770+mocofound@users.noreply.github.com>
2020-04-09 11:34:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "google_compute_instance" "default" {
   }
 
   network_interface {
-    subnetwork = module.network.subnets[0].subnetname
+    subnetwork = "subnet-01"
 
     access_config {
       // Ephemeral IP
